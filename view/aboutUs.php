e13c93d681165c32a91944df9125b94b499f407b
k1748524 <k1748524@kingston.ac.uk>
2019-02-05 12:49:51 +0000

    add image to about us

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -3,6 +3,11 @@
     <head>
         <title>About Us</title>
     </head>
+    <style>
+    img {
+        width: 500px;
+    }
+    </style>
     <body>
         <h1>About Us</h1>
 
@@ -12,7 +17,7 @@
             optional of hiring a professional driver to get you from point A to point B.</br>
             The vehicles come in a veriety or size ranging from a small seven seaters to a large 150 seat double decker bus. </br>
         </p>
-        <img src = "./busHire/van.jpg" />
+        <img src = ".content/images/peoplejump.jpg" />         
         <h2>The Team</h2>
         <p>
             This website was created for a university project, the group behind this project is A14 PATMAN.  
