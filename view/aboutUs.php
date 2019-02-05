1f4a0241732b5ebc5ac80d5782bcb18d9d2b31c3
k1748524 <k1748524@kingston.ac.uk>
2019-02-05 12:54:35 +0000

    atl img on about

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -17,7 +17,7 @@
             optional of hiring a professional driver to get you from point A to point B.</br>
             The vehicles come in a veriety or size ranging from a small seven seaters to a large 150 seat double decker bus. </br>
         </p>
-        <img src = ".content/images/peoplejump.jpg" />         
+        <img src = "../content/images/peoplejump.jpg" />         
         <h2>The Team</h2>
         <p>
             This website was created for a university project, the group behind this project is A14 PATMAN.  
