159f0c5cefb72d299135a42a8be3fd8ae01c0661
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-02-10 13:39:42 +0000

    added the head.php file to the view HTML files so just one head exists for all the files

--- view/index.php
+++ view/index.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link
-      href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" type="text/css" href="../content/css/styles.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <?php require_once "./head.php"; ?>
     <title>🚎 Home Page</title>
   </head>
   <body>
--- view/search.php
+++ view/search.php
@@ -9,16 +9,8 @@ $coaches = getAllCoaches();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link
-      href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700,900"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" type="text/css" href="../content/css/styles.css" />
+    <?php require_once "./head.php"; ?>
     <link rel="stylesheet" type="text/css" href="../content/css/search.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <title>Search Result</title>
 </head>
 <body>
