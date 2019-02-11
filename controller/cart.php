ada8caec7f6920767f09308776b7644f2f9cb7c8
K1450823 <k1450823@kingston.ac.uk>
2019-02-11 18:01:48 +0000

    Sessions working for first vehicle

--- controller/cart.php
+++ controller/cart.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
 require_once "../model/database.php";
-
-$_SESSION["cart"] = $_REQUEST["registrationNumber"];
-$cart = $_SESSION["cart"];
-
+$cart = "";
+if ($_REQUEST["registrationNumber"]==null){
+    $_SESSION["cart"] == 'Empty';
+    $cart = $_SESSION["cart"];
+}
+else{
+    $_SESSION["cart"] = $_REQUEST["registrationNumber"];
+    $cart = $_SESSION["cart"];
+}
 ?>
--- view/checkout.php
+++ view/checkout.php
@@ -1,9 +1,11 @@
+<?php require_once "../controller/cart.php";?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <?php require_once "head.php";?>
-    <?php require_once "../controller/cart.php" ?>
+    
     <title>Document</title>
 </head>
 <?php require_once"header.php";?>
@@ -13,7 +15,7 @@
         <div class="col-sm-8 ">
             <div class="box-outline">
                 <h1>Your basket</h1>
-                <h2><?= $cart ?></h2>
+                <h2><?= $cart; ?></h2>
             </div>
         </div>
         <div class="col-sm-4 ">
