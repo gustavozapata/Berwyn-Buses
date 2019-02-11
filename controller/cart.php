e0d9d576efac516bdc0b1579b43ce0166482bee1
K1450823 <k1450823@kingston.ac.uk>
2019-02-11 19:51:35 +0000

    minor fix

--- controller/cart.php
+++ controller/cart.php
@@ -3,11 +3,11 @@ session_start();
 require_once "../model/database.php";
 
 if (!isset($_REQUEST['regNumber'])&& !isset($_SESSION['cart'])){
-    $_SESSION['cart'][] = "Your Cart is Empty";
+    $_SESSION['cart'][] = 'Empty';
 }
-elseif($_SESSION['cart'][0]=="Your Cart is Empty" && !isset($_REQUEST['regNumber'])){}
+elseif($_SESSION['cart'][0]=='Empty' && !isset($_REQUEST['regNumber'])){}
 
-elseif(isset($_REQUEST['regNumber']) && $_SESSION['cart'][0]=="Your Cart is Empty"){
+elseif(isset($_REQUEST['regNumber']) && $_SESSION['cart'][0]=='Empty'){
     $_SESSION['cart'][0] = $_REQUEST['regNumber'];
 }
 else{
