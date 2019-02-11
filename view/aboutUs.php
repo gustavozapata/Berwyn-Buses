6dfb35fa100b7585569a0cc393b1fcc140b7e691
k1748524 <k1748524@kingston.ac.uk>
2019-02-11 14:59:34 +0000

    css to about

--- view/aboutUs.php
+++ view/aboutUs.php
@@ -9,14 +9,27 @@
       width: 500px;
       border-radius:50%;
     }
+      .borderabout{
+      display: inline-block;
+      width: 400px;
+      height: 280px;
+      margin: 10px 15px;
+      padding: 10px;
+      text-align: center;
+      border: 2px solid rgb(230, 230, 230);
+      border-radius: 20px;
+  /* box-shadow: 0 0 5px rgb(200, 200, 200); */
+    }
     </style>
     <body>
     <div id="page">
       <?php
       require_once "./header.php";
       ?>
-        <h1>About Us</h1>
-        <p>We are dedicated to providing a premium service to all our customers, <br>
+
+        <h1 align = "center"> About Us </h1>
+
+        <p class="borderabout"> We are dedicated to providing a premium service to all our customers, <br>
             we work with all kinds of organisations including business or educational, we also have our serivces available to indviduals. </br>
             There are a variety of high class vehicles that are avalible to hire from, with a wide range of capacities and the additional </br>
             optional of hiring a professional driver to get you from point A to point B.<br>
@@ -24,10 +37,12 @@
         </p>
         <img id = "Gpiece" src = "../content/images/gamepiece.jpeg"/>
 
-        <h2>The Team</h2>
-        <p>
+        <h2 align = "center"> The Team</h2>
+        <p align = "center">
             This website was created for a university project, the group behind this project is A14 PATMAN.  
         </p>
+
+
         <footer>
         <p><a href="#">Design and Developed by A14</a></p>
       </footer>
