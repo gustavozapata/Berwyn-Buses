ee8c7a90eb4c79c9091f795c266dd32e6ee4c9c0
Gustavo Zapata <k1715308@kingston.ac.uk>
2019-02-17 19:07:47 +0000

    added a php file to handle uploads to the server using HTML input type file

--- view/admin_view.php
+++ view/admin_view.php
@@ -58,6 +58,15 @@ require_once "../controller/admin_controller.php";
           <div class="book-coach-header">
             <h2>Coaches</h2>
             <h3>Welcome <?= $admin[0]->givenName ?></h3>
+
+            <!-- UPLOAD FILE TO SERVER -->
+            <form action="../view/submit.php" id="formulario">
+              <input style="width: 300px; border: none" type="file" name="file" id="file"><br>
+              <button type="submit">Upload</button>
+            </form>
+            <div id="progress"></div>
+            <!-- UPLOAD FILE TO SERVER -->
+
           </div>
           <div>
             <form action="../controller/search_controller.php">
@@ -75,5 +84,29 @@ require_once "../controller/admin_controller.php";
     <!-- PAGE -->
     <script src="../scripts/index.js"></script>
     <script src="../scripts/admin.js"></script>
+    
+    <!-- UPLOAD FILE TO SERVER -->
+    <script>
+      var forma = document.getElementById("formulario");
+      var elfile = document.getElementById("file");
+
+      var request = new XMLHttpRequest();
+
+      request.upload.addEventListener("progress", function(e){
+        document.querySelector("#progress").innerHTML = Math.round(e.loaded/e.total * 100) + "%";
+      }, false);
+
+      forma.addEventListener("submit", function(e){
+        e.preventDefault();
+
+        var formData = new FormData();
+        formData.append("file", elfile.files[0]);
+
+        request.open("post", "../view/submit.php");
+        request.send(formData);
+
+      }, false);
+    </script>
+    <!-- UPLOAD FILE TO SERVER -->
   </body>
 </html>
