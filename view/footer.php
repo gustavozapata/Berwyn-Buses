79505e5dc49028d987dfdf6d26a8890be785a3d6
Gustavo Zapata <k1715308@kingston.ac.uk>
2019-02-17 13:08:34 +0000

    added links to the footer

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,9 @@
 <footer>
+    <div class="footer-links">
+        <a href="admin.php">Admin</a>
+        <a href="admin.php">About</a>
+        <a href="admin.php">Brief</a>
+    </div>
     <p><a href="#">Design and Developed by A14</a></p>
     <div class="contact">
         <p>Contact Us</p>
