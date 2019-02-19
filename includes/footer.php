d3c33a6af2a5008c5f54b13376907ab12e000160
James <jpjmarks@gmail.com>
2019-02-19 16:07:07 +0000

    changed conact us frame

--- includes/footer.php
+++ includes/footer.php
@@ -21,16 +21,39 @@
         background-color: rgba(250,250,250,0.8);
     }
     .modal {
-        padding: 10px;
         display: none;
         position: absolute;
-        background-color: rgb(240,240,240);
         top: calc(45vh - 200px);
         left: calc(50% - 125px);
-        height: 400px;
+        height: min-content;
         width: 250px;
         border-radius: 8px;
     }
+    .modal > .modal-header, .modal > .modal-body, .modal > .modal-footer {
+        padding: 10px;
+    }
+
+    .modal > .modal-header {
+        width: 100%;
+        background-color: rgb(240,240,240);        
+    }
+
+    .modal > .modal-body {
+        display: block;
+        height: 250px;
+        width: 100%;
+        background-color: rgb(255,255,255);
+    }
+
+    .modal > .modal-footer {
+        width: 100%;
+        background-color: rgb(240,240,240);
+        position: absolute;
+        bottom: 0;
+        display: flex;
+        justify-content: flex-end;
+    }
+
 </style>
 <div class="faded-background">
 </div>
