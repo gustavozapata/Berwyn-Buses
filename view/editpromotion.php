557edad58189e4312ef4f96bc150ae59a868dbd2
k1748524 <k1748524@kingston.ac.uk>
2019-02-26 09:09:32 +0000

    Add edit promo page and link

--- view/editpromotion.php
+++ view/editpromotion.php
@@ -1,6 +1,8 @@
 <?php 
 
-require_once "../controller/admin_controller.php";
+session_start();
+
+// require_once "../controller/admin_controller.php";
 
 ?>
 
@@ -10,13 +12,13 @@ require_once "../controller/admin_controller.php";
     <?php require_once "../includes/head.php"; ?>
     <link rel="stylesheet" type="text/css" href="../content/css/admin.css" />
     <link rel="stylesheet" type="text/css" href="../content/css/login.css" />
-    <title>Admin Page</title>
+    <title>➕Edit Promotion</title>
   </head>
   <body>
     <div id="page">
       <header>
         <div class="logo">
-          <h1><a href="../view/admin_view.php">Berwyn Buses Hire</a></h1>
+          <h1><a href="../view/index.php">Berwyn Buses Hire</a></h1>
           <span id="movilBasket"></span>
           <div id="line1"></div>
           <div id="line2"></div>
@@ -34,38 +36,21 @@ require_once "../controller/admin_controller.php";
         <article class="book-coach">
           <?php if(!$_SESSION["adminLogged"]): ?>
           <div class="book-coach-header">
-            <h2>Admin Login</h2>
-            <?php if(isset($_REQUEST["adminname"])): ?>
-            <p id="loginStatus">Username or password incorrect</p>
-            <?php endif; ?>
-          </div>
-          <div>
-            <form action="../controller/admin_controller.php" method="post">
-              <span>
-                <p>Username</p>
-                <input type="name" name="adminname" required/>
-              </span><br>
-              <span>
-                <p>Password</p>
-                <input type="password" name="password" required/>
-              </span>
-              <br />
-              <input type="submit" value="Login" />
-            </form>
+            <h2><a href="../view/admin_view.php">OOPS Login!</a></h2>
           </div>
           <?php else: ?>
           <div class="book-coach-header">
-            <h2>Coaches</h2>
-            <div class="admin-buttons">
-              <a href="../view/addcoach.php">Add coach</a>
-              <a href="#">Edit coach</a>
+            <h2>Edit Promotion</h2>
+            <form action="">
+                <input type="text" placeholder="Promotion ID">
+                <input type="text" placeholder="Promotion Descripton">
+                <span>
+                <input type="number" placeholder="Discount Amount"/>
+              </span>
+                <div class="admin-buttons">
+              <a href="#">Add</a>
             </div>
-
-            <h2>Promotions</h2>
-            <div class="admin-buttons">
-              <a href="../view/addpromotion.php">Add promotion</a>
-              <a href="../view/editpromotion.php">Edit promotion</a>
-            </div>
+            </form>
 
           </div>
           <?php endif ?>
