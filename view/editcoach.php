4b8c9b90bce97e4306163e8bd1f7b7426bc28822
k1711288 <k1711288@kingston.ac.uk>
2019-02-26 09:17:16 +0000

    Added editcoach page and updated the editpromotion page.

--- view/editcoach.php
+++ view/editcoach.php
@@ -12,7 +12,7 @@ session_start();
     <?php require_once "../includes/head.php"; ?>
     <link rel="stylesheet" type="text/css" href="../content/css/admin.css" />
     <link rel="stylesheet" type="text/css" href="../content/css/login.css" />
-    <title>➕Edit Promotion</title>
+    <title>🔧Edit Coaches</title>
   </head>
   <body>
     <div id="page">
@@ -23,32 +23,28 @@ session_start();
           <div id="line1"></div>
           <div id="line2"></div>
         </div>
-        <?php if($_SESSION["adminLogged"]): ?>
+        <?php if($_SESSION["isLogged"]): ?>
         <nav>
           <ul>
-            <li><a href="../controller/logout.php">Logout</a></li>
-            <li><a href="../view/admin_view.php"><?= $_SESSION["adminname"]?></a></li>
+            <li><a href="../controller/admin_logout.php">Logout</a></li>
+            <li><a href="../view/admin_view.php"><?= $_SESSION["username"]?></a></li>
           </ul>
         </nav>
         <?php endif; ?>
       </header>
       <section class="main-section">
         <article class="book-coach">
-          <?php if(!$_SESSION["adminLogged"]): ?>
+          <?php if(!$_SESSION["isLogged"]): ?>
           <div class="book-coach-header">
             <h2><a href="../view/admin_view.php">OOPS Login!</a></h2>
           </div>
           <?php else: ?>
           <div class="book-coach-header">
-            <h2>Edit Promotion</h2>
+            <h2>Edit Coaches</h2>
             <form action="">
-                <input type="text" placeholder="Promotion ID">
-                <input type="text" placeholder="Promotion Descripton">
-                <span>
-                <input type="number" placeholder="Discount Amount"/>
-              </span>
+
                 <div class="admin-buttons">
-              <a href="../view/admin_view.php">Add</a>
+              <a href="../view/admin_view.php">Save Changes</a>
             </div>
             </form>
 
