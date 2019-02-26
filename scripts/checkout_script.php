fe8b92c82aa4d50d973d9046bcabafa23d7f72e8
K1450823 <k1450823@kingston.ac.uk>
2019-02-26 09:11:56 +0000

    added comments to checkout_script

--- scripts/checkout_script.php
+++ scripts/checkout_script.php
@@ -1,6 +1,5 @@
 <script>
-var text = '<?= $_SESSION['cart'][0] ?>';
-console.log('text: '+text);
+var text = '<?= $_SESSION['cart'][0] ?>';//checks if the first value is empty
     if(text == '' ){
         $('div#image').replaceWith();
         $('div.basketInfo').replaceWith('<div class="col-sm-12"><h3 style="text-align:center">Your cart is empty</h3></div>');
